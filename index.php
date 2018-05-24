6d7883308ecfa55f06e519ccd12c1c28d76cf1ef
arvin <1095654362@qq.com>
2018-05-24 17:40:47 +0800

    添加背景图片

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="zh-CN">
 	<head>
 		<meta charset="utf-8" />
 		<title>个人博客</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<style type="text/css">
+			
+		</style>
 	</head>
 	<body>
-		<script color="0,0,255" opacity='0.7' zIndex="-2" count="70" src="js/canvas-nest.js" type="text/javascript" charset="utf-8"></script>
-		<div class="container" id="">
-			
-		</div>
+		<script color="0,0,255" opacity='0.7' zIndex="-1" count="59" src="js/canvas-nest.js" type="text/javascript" charset="utf-8"></script>
+		
 	</body>
 </html>
