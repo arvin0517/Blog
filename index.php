21829aeddd2cb96d1d3a7d029cb69f017a09a700
arvin <1095654362@qq.com>
2018-05-25 17:24:29 +0800

    更新

--- index.php
+++ index.php
@@ -7,12 +7,30 @@
 		<meta charset="utf-8" />
 		<title>个人博客</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" type="text/css" href="css/myanimate.css"/>
 		<style type="text/css">
 			
 		</style>
 	</head>
 	<body>
-		<script color="0,0,255" opacity='0.7' zIndex="-1" count="59" src="js/canvas-nest.js" type="text/javascript" charset="utf-8"></script>
-		
+		<script color="243,142,40" opacity='0.7' zIndex="-1" count="59" src="js/canvas-nest.js" type="text/javascript" charset="utf-8"></script>
+		<div id="container" class="container">
+			<div class="header">
+			   <img class="header_logo float-l leftInto" src="img/my/logo.png"/>
+			   <div class="header_text float-l rightInto" id="">
+				   <div id="" class="title">本的个人博客</div>
+				   <div id="" class="description">一个懒人不想做但不得不做的博客</div>			   	
+			   </div>
+			</div>
+			<nav>
+				<ul>
+					<li><a class="active" href="">技术博文</a></li>
+					<li><a href="">前端作品</a></li>
+					<li><a href="">碎碎念</a></li>
+					<li><a href="">留言板</a></li>
+					<li><a href="">关于我</a></li>
+				</ul>
+			</nav>
+		</div>
 	</body>
 </html>
